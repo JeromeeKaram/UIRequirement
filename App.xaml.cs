d5a038362ad92b025e04fccebde58143a63d9757
Ganikapudi, Shamsundar (Cyient) <shamsundar.ganikapudi@alcoa.com>
2026-07-23 15:05:49 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UIRequirement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
